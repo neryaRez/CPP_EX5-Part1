02bf7b6f311767938cc1b2e3c6ec29d7c5cfc657
neryaRez <neryarez@gmail.com>
2023-06-02 12:20:38 +0300

    mag

---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstdbool>
+
+using namespace std;
+
+namespace ariel{
+
+    class MagicalContainer
+    {
+    private:
+    vector<int>& data;
+    vector<int*>& prines;
+        
+    public:
+        MagicalContainer(/* args */);
+        MagicalContainer();
+        ~MagicalContainer();
+
+        
+
+
+
+
+
+    };
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
